22ae3da0b003161a1d77567b04c7be79f6969367
Jarka Baker <jarka.baker.1@gmail.com>
2020-10-01 21:30:03 +0100

    Java 12 - String - Indent

--- src/com/tardis/stringMethods/StringMethodTester.java
+++ src/com/tardis/stringMethods/StringMethodTester.java
@@ -7,6 +7,7 @@ public class StringMethodTester {
 		System.out.println();
 		System.out.println("String Method Tests");
 		this.testStringRepeat();
+		this.testIndent();
 		
 	}
 	
@@ -22,6 +23,20 @@ public class StringMethodTester {
 		System.out.println(sayHelloA);
 		System.out.println(sayHelloB);
 		
+	}
+	
+	private void testIndent() {
+		
+		String inputString = "I am Czech";
+		
+		String output1 = inputString.indent(5);
+		System.out.println("Testind Indent 5");
+		System.out.println(output1);
+		
+		String output2 = output1.indent(-2);
+		System.out.println("Testind Indent -2");
+		System.out.println(output2);
+    
 	}
 
 }
